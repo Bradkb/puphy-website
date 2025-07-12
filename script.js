0c66359c087318bbfcc8592348b0540182d3ec19
Bradkb <96817962+Bradkb@users.noreply.github.com>
2025-07-12 12:14:50 +0200

    new

--- script.js
+++ script.js
@@ -1,5 +1,1 @@
-document.getElementById('contactForm').addEventListener('submit', function (e) {
-  e.preventDefault();
-  alert('Thank you for contacting Puphy. We will get back to you soon!');
-  this.reset();
-});
+document.getElementById('contactForm').addEventListener('submit', function (e) { e.preventDefault(); alert('Thanks for reaching out!'); });
